4c72e1a795db75b01c8b5090cb3a32f20e89f337
Fred Mitchell <lordalveric@yahoo.com>
2016-02-17 13:42:47 +0100

    fixed some bugs, implemented all play opts

--- lib/ansible-powerplay/powerplay.rb
+++ lib/ansible-powerplay/powerplay.rb
@@ -3,22 +3,33 @@ require 'ansible-powerplay'
 
 module Powerplay
   module Play
+    DEFOUT = "&1" # default non-tmux output
+
+    def self.clopts
+      @cliots ||= DSL::_global[:options]
+    end
+
     module Tmux
       # get a list of the ptys 
       def self.pane_ptys
-        @ptys ||= %x[tmux list-panes -F '\#{pane_tty},']
-          .inspect
-          .chop
-          .split(",")
-          .map{ |s| s.strip.sub(/\\n|\"/, '') }
+        @ptys ||= if Play::clopts[:tmux]
+                    %x[tmux list-panes -F '\#{pane_tty},']
+                      .inspect
+                      .chop
+                      .split(",")
+                      .map{ |s| s.strip.sub(/\\n|\"/, '') }
+                  else
+                    [Play::DEFOUT]
+                  end
       end
     end
 
     module Ansible
       PLAYBOOK = "ansible-playbook"
       OPTS = ""
+
       def self.playbooks
-        play = DSL::_global[:options][:play].to_sym
+        play = Play::clopts[:play].to_sym
         if play == :all
           DSL::_global[:playbooks].each do |play, group|
             yield play, group
@@ -30,36 +41,43 @@ module Powerplay
       
       # groups are serial
       def self.groups(playbook)
+        grp = Play::clopts[:group].to_sym
         playbook.groups.each do |group|
-          yield group
+          yield group if grp == :all or grp == group.type  
         end
       end
 
       def self.power_run
+        buch = Play::clopts[:book].to_sym
+        dryrun = Play::clopts[:dryrun]
+        
         playbooks do |pname, playbook|
-          puts "PLAYBOOK #{pname} -->"
+          puts "PLAYBOOK #{pname} (group=#{Play::clopts[:group]}) -->"
           groups playbook do |group|
-            puts "    GROUP #{group.type} -->"
+            puts "    GROUP #{group.type} (book=#{buch}) -->"
             thrs = []
             errors = []
             group.books.zip(Tmux.pane_ptys) do |book, tty|
-              tty ||= Tmux.pane_ptys.last 
-              apcmd = %|#{PLAYBOOK} #{OPTS} #{book.config[:playbook_directory].first}/#{book.yaml} --extra-vars "#{book.aparams}" >#{tty}|
-              thrs << Thread.new {
-                std, status = Open3.capture2e apcmd
-                errors << [book.yaml, apcmd, std] unless status.success?
-              }
-              thrs.each{ |t| t.join }
-              unless errors.empty?
-                errors.each do |yaml, cmd, txt|
-                  puts '=' * 30
-                  puts ('*' * 10) + ' ' + yaml
-                  puts txt
-                  puts '-' * 30
-                  puts cmd
-                end
-                exit 10
+              tty ||= Tmux.pane_ptys.last
+              if buch == :all or book.type == buch
+                puts "        BOOK #{book.type}"
+                apcmd = %|#{PLAYBOOK} #{OPTS} #{book.config[:playbook_directory].first}/#{book.yaml} --extra-vars "#{book.aparams}" >#{tty}|
+                thrs << Thread.new {
+                  std, status = Open3.capture2e apcmd
+                  errors << [book.yaml, apcmd, std] unless status.success?
+                } unless dryrun
               end
+            end
+            thrs.each{ |t| t.join }
+            unless errors.empty?
+              errors.each do |yaml, cmd, txt|
+                puts '=' * 30
+                puts ('*' * 10) + ' ' + yaml
+                puts txt
+                puts '-' * 30
+                puts cmd
+              end
+              exit 10
             end
           end
         end
