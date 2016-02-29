37513869eae687014653a6a17c838d2cd6b05d84
Fred Mitchell <lordalveric@yahoo.com>
2016-02-29 19:34:41 +0100

    made play script specification optional

--- lib/ansible-powerplay/cli.rb
+++ lib/ansible-powerplay/cli.rb
@@ -7,11 +7,14 @@ module Powerplay
     class Main < Thor
       class_option :verbose, type: :numeric, banner: '[1|2|3]', aliases: '-v', default: 0
 
-      desc 'play <script>', 'Run the powerplay script.'
+      desc 'play [script]', 'Run the powerplay script.'
       long_desc <<-LONGDESC
-        Plays a powerscript. The entries in the
-        script, as specified, are run in parallel
-        by default. 
+        Plays a PowerPlay script. The entries in the
+        script, as specified inside of a group, are run
+        in parallel by default.
+
+        if [script] is not given, it defaults to 'stack.play'
+        in the current directory.
       LONGDESC
       option :tmux,      type: :numeric, lazy_default: 0, aliases: '-m', banner: "[WINDOWNUMBERopt] Send output to all tmux panes in the current window, or the numeric window specified."
       option :play,      type: :array,  aliases: '-p', banner: "[NAME[ NAME2...]|all] Which playbook shelves", required: true
@@ -19,7 +22,7 @@ module Powerplay
       option :congroups, type: :boolean, aliases: '-c', banner: "Run the groups themselves concurrently"
       option :book,      type: :array,  aliases: '-b', banner: "[NAME[ NAME2...]|all] Which books to execute", default: [:all]
       option :dryrun,    type: :boolean, aliases: '-u', banner: "Dry run, do not actually execute."
-      def play(script)
+      def play(script = 'stack.play')
         DSL::_global[:options] = options
         puts "script %s " % [script] if DSL::_global[:options][:verbose] >= 1
         load script, true
