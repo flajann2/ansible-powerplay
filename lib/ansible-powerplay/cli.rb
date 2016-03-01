f3fa8789ff76d9d394ae70fdfea25a2e95293a06
Fred Mitchell <lordalveric@yahoo.com>
2016-03-01 11:15:23 +0100

    clean up of help, new --extra-vars option to pass custom parameters to playbooks

--- lib/ansible-powerplay/cli.rb
+++ lib/ansible-powerplay/cli.rb
@@ -16,12 +16,19 @@ module Powerplay
         if [script] is not given, it defaults to 'stack.play'
         in the current directory.
       LONGDESC
-      option :tmux,      type: :numeric, lazy_default: 0, aliases: '-m', banner: "[WINDOWNUMBERopt] Send output to all tmux panes in the current window, or the numeric window specified."
-      option :play,      type: :array,  aliases: '-p', banner: "[NAME[ NAME2...]|all] Which playbook shelves", required: true
-      option :group,     type: :array,  aliases: '-g', banner: "[NAME[ NAME2...]|all] Which groups to execute", default: [:all]
-      option :congroups, type: :boolean, aliases: '-c', banner: "Run the groups themselves concurrently"
-      option :book,      type: :array,  aliases: '-b', banner: "[NAME[ NAME2...]|all] Which books to execute", default: [:all]
-      option :dryrun,    type: :boolean, aliases: '-u', banner: "Dry run, do not actually execute."
+      option :tmux,      type: :numeric, aliases: '-m', banner: "[WINDOWNUMBERopt]", lazy_default: 0,
+                                                        desc: ' Send output to all tmux panes in the current window, or the numeric window specified.'
+      option :play,      type: :array,   aliases: ['-p', '--play', '--power'], banner: "[NAME[ NAME2...]|all]", required: true,
+                                                        desc: 'Which powerplays to specifically execute.'
+      option :group,     type: :array,   aliases: '-g', banner: "[NAME[ NAME2...]|all]", default: [:all],
+                                                        desc: ' Which groups to execute.'
+      option :congroups, type: :boolean, aliases: '-c', desc: "Run the groups themselves concurrently"
+      option :book,      type: :array,   aliases: '-b', banner: "[NAME[ NAME2...]|all]", default: [:all],
+                                                        desc: 'Which books to execute.'
+      option :dryrun,    type: :boolean, aliases: '-u', desc: "Dry run, do not actually execute."
+      option :extra,     type: :array,   aliases: ['-x', '--extra-vars'],
+                                                        banner: %(<BOOKNAME|all>:"key1a=value1a key2a=value2a... " [BOOKNAME2:"key1b=value1b key2b=value2b... "]),
+                                                        desc: 'Pass custom parameters directly to playbooks'
       def play(script = 'stack.play')
         DSL::_global[:options] = options
         puts "script %s " % [script] if DSL::_global[:options][:verbose] >= 1
